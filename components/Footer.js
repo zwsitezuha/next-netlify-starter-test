17711f50a6b01fc4aa9905c6ecbce08abe5526d3
ZwS <149260448+zwsitezuha@users.noreply.github.com>
2025-01-03 16:31:24 +0600

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <>
       <footer className={styles.footer}>
-        <img src="/logo-netlify.svg" alt="Netlify Logo" className={styles.logo} />
+        <img src="/zwnt-logo.svg" alt="Netlify Logo" className={styles.logo} />
       </footer>
     </>
   )
