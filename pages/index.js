0249ec858e38366f685031bf7299f74ac3ee0ff9
ZwS <149260448+zwsitezuha@users.noreply.github.com>
2025-01-03 16:24:23 +0600

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,9 +11,9 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Welcome to ZwSite App!" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+         You can start by editing <code>pages/index.js</code>
         </p>
       </main>
 
